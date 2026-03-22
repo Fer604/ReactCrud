3e6052cb2b50ab1f6cc407f0ad09d1b3784d54b9
Fer604 <fernando.as604@gmail.com>
2026-03-22 04:23:41 -0300

    json tava errado

--- src/back/index.js
+++ src/back/index.js
@@ -1,2 +1,12 @@
 import express from 'express';
-const app = express
+import db from '../db/config';
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+
